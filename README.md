531b610909de9bbfeaaa22518f7d8909c6e0107e
liangguanghui <liangguanghui@keep.com>
2022-03-24 13:55:11 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AlgorithmX
+
+A description of this package.
---
+++ Tests/AlgorithmXTests/AlgorithmXTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import AlgorithmX
+
+final class AlgorithmXTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(AlgorithmX().text, "Hello, World!")
+    }
+}
